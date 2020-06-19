5d5fe9a8c0c8d110b9907b72172c1a0199a7e3e1
Evan Godon <e.godon@outlook.com>
2020-06-19 18:11:01 -0400

    Fri Jun 19 18:11:01 EDT 2020

--- fish/functions/journal.fish
+++ fish/functions/journal.fish
@@ -1,5 +1,5 @@
 function journal -d "Start a new journal entry"
-    echo \n(date +"%a %b %d, %Y")\n--- >> ~/work/notes/journal.md
+    echo \n(date +"%a %b %d, %Y")\n--- >> ~/work/lane-notes/Journal.md
 
-    nvim ~/work/notes/journal.md
+    nvim ~/work/lane-notes/Journal.md
 end
--- fish/functions/jrnl.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function jrnl --wraps=journal --description 'alias jrnl journal'
-  journal  $argv;
-end

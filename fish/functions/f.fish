11e21b5ad3efdec74ef9dc8a98e3b957c482e1e1
Evan Godon <e.godon@outlook.com>
2020-07-12 00:29:56 -0400

    Update

--- fish/functions/f.fish
+++ fish/functions/f.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
-function --description 'Fuzzy search for a directory and cd into it.'
+function f --description 'Fuzzy search for a directory and cd into it.'
   set directory  (fd --type directory | fzf);
 
   if test -z "$directory";

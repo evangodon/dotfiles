12b70d0ac0988a20ede360ef8cb38196ae65a9e9
Evan Godon <e.godon@outlook.com>
2020-11-01 21:10:33 -0500

    update ll command

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,4 +1,8 @@
 # Defined in - @ line 1
-function ll --wraps='lsd -l' --description 'alias ll lsd -l'
-  lsd -l $argv;
+function ll --description 'alias ll to lsd -l or ls -alh'
+  if type -q lsd
+   lsd -l $argv;
+  else
+    ls -alh
+  end
 end

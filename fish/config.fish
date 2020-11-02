09c3a455f9d12328e35171406e3d2812d47e9dbd
Evan Godon <e.godon@outlook.com>
2020-11-01 21:37:02 -0500

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,10 @@ set fish_greeting ""
 # Keyboard Shortcuts
 bind \co fzf
 
-starship init fish | source
+if type -q starship
+  starship init fish | source
+end
+
 
 set -x FZF_DEFAULT_COMMAND "rg --files --hidden --glob \"!{node_modules,.git}\""
 
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,7 @@
 function fish_user_key_bindings
-  fzf_key_bindings
+  if type -q fzf_key_bindings
+    fzf_key_bindings
+  end
   bind \eg 'test -d .git; and git checkout (string trim -- (git branch | fzf)); and commandline -f repaint'
   bind \eG 'test -d .git; and git checkout (string trim -- (git branch --all | fzf)); and commandline -f repaint'
 end

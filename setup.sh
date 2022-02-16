8ec0036784f263525b2f1f84eeea767ec5f20c6a
Evan Godon <e.godon@outlook.com>
2022-02-15 23:05:29 -0500

    Add nix setup script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+# Install nix package manager
+curl -L https://nixos.org/nix/install | sh
+
+# Install packages
+nix-env -iA nixpkgs.fish    \
+nixpkgs.fzf                 \
+nixpkgs.fd                  \
+nixpkgs.lazygit             \
+nixpkgs.pgcli               \
+nixpkgs.ncdu                \
+nixpkgs.bottom              
+
+# Change default shell to fish
+
+# Install fisher
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+# Install fisher plugins
+fisher install         \
+jethrokuan/z           \
+jorgebucaran/nvm.fish  \
+PatrickF1/fzf.fish     \
+franciscolourenco/done \
+victante/trish         \
+lilyball/nix-env.fish
+
+# Install kitty terminal
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin

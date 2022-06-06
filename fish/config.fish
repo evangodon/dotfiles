5309ed700e0fc8310ca29271f479536c2d70ace6
Evan Godon <e.godon@outlook.com>
2022-06-06 16:20:31 -0400

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -33,7 +33,9 @@ end
 set -gx EDITOR (type -p nvim)
 
 # nvm
-set -gx nvm_default_version 14 
+if type -q (which node)
+  set -gx nvm_default_version 16
+end
 
 # Set path for kb cli
 set -gx KB_PATH /home/evan/notes/knowledge-base
@@ -48,6 +50,27 @@ set -gx XDG_DATA_DIRS /usr/share/:/usr/local/share/:/var/lib/snapd/desktop
 # Secrets 
 if test -e $__fish_config_dir/.env.local
     source $__fish_config_dir/.env.local
+end
+
+# GoLang
+set GOROOT '/Users/evan.godon/.go'
+set GOPATH '/Users/evan.godon/go'
+set PATH $GOPATH/bin $GOROOT/bin $PATH
+
+switch (uname)
+    case Linux
+      return
+    case Darwin
+      # Homebrew
+      set -gx HOMEBREW_PREFIX "/opt/homebrew";
+      set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+      set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+      set -q PATH; or set PATH '';
+      set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+      set -q MANPATH; or set MANPATH '';
+      set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+      set -q INFOPATH; or set INFOPATH '';
+      set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
 end
 
 

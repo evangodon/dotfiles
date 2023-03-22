d35816e66ca7397e64a922561c74a2b6c5e09436
Evan Godon <evan.godon@lightspeedhq.com>
2023-03-22 15:59:14 -0400

    update how fonts are installed with kitty

--- scripts/install_packages.mac.sh
+++ scripts/install_packages.mac.sh
@@ -1,2 +1,12 @@
+# Install nerd font symboles
+brew install --cask font-symbols-only-nerd-font
+
+# Install Jetbrains Mono font
+brew tap homebrew/cask-fonts
+brew install --cask font-jetbrains-mono
+
+
+
+# Jira
 brew tap ankitpokhrel/jira-cli
 brew install jira-cli

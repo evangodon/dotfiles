881ab97ad2be48ccd82c9293dbc7e4e7a05f6742
evangodon <e.godon@outlook.com>
2024-11-09 19:41:48 -0500

    fix customer fzf script so that it displays dot files

--- dot_config/fish/exact_functions/custom_fzf_search_dir.fish
+++ dot_config/fish/exact_functions/custom_fzf_search_dir.fish
@@ -8,9 +8,10 @@ function custom_fzf_search_dir  --description "Search for a file in a directory 
     set -q _flag_directory && set -l dir $_flag_directory
 
     set_color blue; echo "Searching files in $dir"
-    set file (fd . $extension_flag $dir | fzf --preview-window noborder \
+    set file (fd -H -E .git -E fonts -E bin  . $extension_flag $dir | fzf --preview-window noborder \
                               --delimiter $dir --with-nth -1 \
                               --height=85% \
+                              --margin=0,3,0,1 \
                               --margin=0,3,0,1 \
                               --preview "custom_fzf_preview_window {}")
 

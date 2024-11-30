37c7132361ef929e14b3ed90899d9a1814dca4b1
Evan Godon <evan.godon@lightspeedhq.com>
2024-11-29 19:38:30 -0500

    update kitty tab bar script

--- dot_config/kitty/tab_bar.py
+++ dot_config/kitty/tab_bar.py
@@ -2,24 +2,12 @@
 # pyright: reportMissingImports=false
 # pylint: disable=E0401,C0116,C0103,W0603,R0913
 
+from kitty.rgb import Color
 from kitty.fast_data_types import Screen, get_options, get_boss
 from kitty.utils import color_as_int
 from kitty.tab_bar import DrawData, ExtraData, TabBarData, draw_title, as_rgb
 
 opts = get_options()
-
-# Dictionary of tab colors
-tab_colors = {
-    1: opts.color9, 
-    2: opts.color12,
-    3: opts.color10,
-    4: opts.color13,
-    5: opts.active_tab_background,
-    6: opts.active_tab_background,
-    7: opts.active_tab_background,
-    8: opts.active_tab_background,
-    9: opts.active_tab_background,
-}
 
 mappings = {
     1: "ʰ", 
@@ -27,7 +15,6 @@ mappings = {
     3: "ᵏ",
     4: "ˡ",
 }
-
 
 def to_sup(s):
     sups = {u'0': u'\u2070',
@@ -43,48 +30,62 @@ def to_sup(s):
 
     return ''.join(sups.get(char, char) for char in s)
 
+def darken_color(color: Color, factor: float) -> Color:
+    r, g, b = color.r, color.g, color.b
+
+    r = max(0, int(r * (1 - factor)))
+    g = max(0, int(g * (1 - factor)))
+    b = max(0, int(b * (1 - factor)))
+
+    return Color(r, g, b)
+
 def get_active_tab_index() -> int:
     return get_boss().active_tab_manager.active_tab_idx + 1
+
+inactive_tab_bg = as_rgb(color_as_int(opts.inactive_tab_background))
+white_text_color = as_rgb(color_as_int(opts.color7))
+red_color = opts.color9
+primary_color = as_rgb(color_as_int(red_color))
+primary_color_dark = as_rgb(color_as_int(darken_color(red_color, 0.2)))
 
 def draw_tab(
     draw_data: DrawData, screen: Screen, tab: TabBarData,
     before: int, max_title_length: int, index: int, is_last: bool,
     extra_data: ExtraData
 ) -> int:
+    active_tab_idx = get_active_tab_index()
+    tab_is_active = index == active_tab_idx
+    next_tab_is_active = index + 1 == active_tab_idx
+    map_indicator = mappings.get(index, to_sup(str(index)))
+    tab_needs_attention = tab.needs_attention
+
+    # Colors
     orig_fg = screen.cursor.fg
     orig_bg = screen.cursor.bg
-    left_sep, right_sep = ('', '')
-    active_tab_idx = get_active_tab_index()
-    tabIsActive = index == active_tab_idx
-    nextTabIsActive = index + 1 == active_tab_idx
-    default_bg = as_rgb(int(draw_data.default_bg))
-    inactive_tab_bg = as_rgb(color_as_int(opts.inactive_tab_background))
-
-    custom_bg = as_rgb(color_as_int(opts.color9))
-    next_tab_custom_bg = as_rgb(color_as_int(opts.color9)) 
-    tab_bg = custom_bg if tabIsActive else orig_bg
-
+    default_bg = as_rgb(color_as_int(draw_data.default_bg))
 
     def draw_sep() -> None:
-        if is_last:
-            separator_bg = default_bg
-        elif nextTabIsActive:
-            separator_bg = next_tab_custom_bg
+        if is_last and tab_is_active:
+            separator_bg = primary_color_dark
+        elif is_last:
+            separator_bg = inactive_tab_bg
+        elif next_tab_is_active:
+            separator_bg = primary_color_dark
         else:
             separator_bg = inactive_tab_bg
 
-        if tabIsActive:
-           separator_fg = custom_bg
-        elif not nextTabIsActive:
-           separator_fg = custom_bg
+        if is_last:
+           separator_fg = default_bg
+        elif not next_tab_is_active:
+           separator_fg = primary_color_dark
         else:
            separator_fg = inactive_tab_bg
 
         screen.cursor.bg = separator_bg
         screen.cursor.fg = separator_fg
 
-        separator_char = '' if tabIsActive or nextTabIsActive or is_last else "╱"
-        screen.draw(separator_char) if not is_last else None
+        separator_char = '' if tab_is_active or next_tab_is_active or is_last else "╱"
+        screen.draw(separator_char) if not is_last else screen.draw('')
 
 
     if max_title_length <= 1:
@@ -93,10 +94,12 @@ def draw_tab(
         screen.draw('…|')
     else:
         screen.draw('')
-        screen.cursor.bg = tab_bg
+        screen.cursor.bg = primary_color_dark if tab_is_active else inactive_tab_bg
+        screen.cursor.fg = white_text_color if tab_is_active else primary_color 
+        screen.draw('  ') if tab_needs_attention else screen.draw('   ')
         draw_title(draw_data, screen, tab, index)
-        map_indicator = mappings.get(index, to_sup(str(index)))
-        screen.draw(f'{map_indicator} ')
+        screen.draw(f' {map_indicator}')
+        screen.draw('  ')
         draw_sep()
         screen.cursor.bg = orig_bg
         screen.cursor.fg = orig_fg

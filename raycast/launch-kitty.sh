5398ca321ec42d1dbbd54b1ffbd7ecd97dcc8b70
Evan Godon <evan.godon@lightspeedhq.com>
2025-04-26 15:40:42 -0400

    add launch kitty script for raycast

---
+++ raycast/launch-kitty.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Launch Kitty 
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🐱
+
+open -n -a Kitty
+
